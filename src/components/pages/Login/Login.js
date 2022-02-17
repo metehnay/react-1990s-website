c6a69a441ad48d14ab9e9b9f35af6809463cfc1e
metehnay <oguzwst@gmail.com>
2022-02-17 19:50:00 +0300

    sign up fix

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,11 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 import { auth, provider } from "../../../firebase-config";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+
 import { signInWithPopup } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 
 const Login = ({ setIsAuth, isAuth }) => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
   let navigate = useNavigate();
+
+  const logged = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      localStorage.setItem("isAuth", true);
+
+      setIsAuth(true);
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const signIn = () => {
     signInWithPopup(auth, provider).then((result) => {
@@ -17,12 +43,55 @@ const Login = ({ setIsAuth, isAuth }) => {
   };
 
   return (
-    <div className="login mt-16 bg-red-500">
-      <div className="login1">
-        <p>If Popup didn't show up. U can use this button</p>
-        <button onClick={signIn}>Sign In</button>
+    <>
+      <div className="login mt-16">
+        <div className="login1">
+          <button
+            onClick={signIn}
+            className="mb-4 bg-yellow-400 p-2 rounded text-dark"
+          >
+            Sign In with Google
+          </button>
+        </div>
       </div>
-    </div>
+
+      <Card className="w-100 custom-con">
+        <Card.Body className="d-flex justify-content-center" id="custom">
+          <Form onSubmit={logged}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                onChange={(event) => {
+                  setLoginEmail(event.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={(event) => {
+                  setLoginPassword(event.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Button
+              className="w-100 mt-3 bg-warning text-dark border-none"
+              type="submit"
+              onClick={logged}
+            >
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Don't have an account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
   );
 };
 
--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import "./SignUp.css";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+import { auth } from "../../../firebase-config";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+
+  const [user, setUser] = useState({});
+
+  const handleAction = (id) => {
+    id.preventDefault();
+    const authentication = getAuth();
+
+    createUserWithEmailAndPassword(authentication, email, password).then(
+      navigate("/login")
+    );
+  };
+
+  return (
+    <>
+      <Card className="w-100 custom-con">
+        <Card.Body className="d-flex justify-content-center" id="custom">
+          <Form onSubmit={handleAction}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Button
+              className="w-100 mt-3 bg-warning text-dark"
+              type="submit"
+              onClick={handleAction}
+            >
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
