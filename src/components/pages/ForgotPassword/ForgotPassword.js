b84bd522b4f39d1c66adcfb7e8425abf757b8d50
metehnay <oguzwst@gmail.com>
2022-02-19 18:27:15 +0300

    firebase forgot password added.

--- src/components/pages/ForgotPassword/ForgotPassword.js
+++ src/components/pages/ForgotPassword/ForgotPassword.js
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./ForgotPassword.css";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth, provider } from "../../../firebase-config";
 
 const ForgotPassword = () => {
+  const emailRef = useRef();
+
   const [pass, setPass] = useState("");
 
-  const forgotPass = async (e) => {
-    e.preventDefault();
-    try {
-      const user = await sendPasswordResetEmail(auth);
-    } catch (error) {
-      console.log(error.message);
-    }
+  const forgotPass = (email) => {
+    return sendPasswordResetEmail(auth, email);
   };
+
+  const getPass = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    if (email)
+      forgotPass(email).then(() => {
+        emailRef.current.value = "";
+      });
+  };
+
   return (
     <>
       <Card className="w-100 custom-con">
         <Card.Body className="d-flex justify-content-center" id="custom">
-          <Form onSubmit={forgotPass}>
+          <Form>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" required />
+              <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
 
             <Button
               className="w-100 mt-3 bg-warning text-dark border-none"
               type="submit"
+              onClick={getPass}
             >
               Reset
             </Button>
